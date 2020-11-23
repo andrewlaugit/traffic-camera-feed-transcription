44d0f8291f0395f81eaa5ffbe0881c976d9d3111
dermarkr <45435121+dermarkr@users.noreply.github.com>
2020-11-23 14:52:26 -0500

    fixed image_extraction bug preventing
    fixed bug preventing saving of videos with none integer frame rates

--- VideoProcessing/imageextraction.py
+++ VideoProcessing/imageextraction.py
@@ -8,7 +8,9 @@ import time
 
 # requires that video be AVI format
 def image_extraction(video_path = None, image_per_second = 5, target_height = 128, target_width = 256):
-
+    """
+    Checks validity of provided video file path
+    """
     if video_path == None:
         print("No video file provided")
         return False
@@ -25,46 +27,57 @@ def image_extraction(video_path = None, image_per_second = 5, target_height = 12
 
     video_name = video_name.split(".")
 
-    if(video_name[1] != "avi" or video_name[1] != "mkv"):
-        print("Invalid File Format. Inputs most be AVI")
-        return False
+    # if(video_name[1] != "avi" or video_name[1] != "mkv"):
+    #     print("Invalid File Format. Inputs most be AVI")
+    #     return False
 
+
+    """
+    loads video
+    """
     video = VideoCapture(video_path)
 
     if not video.isOpened():
         print("Unable to open file. Please ensure input video is either .AVI or .MKV")
         return False
 
+    
+    """
+    gets video details
+    """
     total_video_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
     frame_height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
     frame_width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
     video_fps = video.get(cv2.CAP_PROP_FPS)
+    video_length = total_video_frames / video_fps
     print(total_video_frames)
     print(frame_height)
     print(frame_width)
-    print(video_fps)
-    video_length = total_video_frames / video_fps
+    print(video_fps)  
     print(video_length)
 
+    """
+    gets working directory
+    """
     current_path = os.getcwd()
 
+    """
+    ensures image folder exists in project root directory
+    """
     if not path.isdir(current_path + r"\images"):
         os.mkdir(current_path + r"\images")
 
     image_path = current_path + r"\images"
 
-    frame_count = 0
-
-    image_per_second = 5 ######Number of frames extracted per second
-    target_height = 128
-    target_width = 256
-
-    scale = (target_width, target_height)
-
-    save_interval = video_fps / image_per_second
-
     begin = time.time()
 
+    """
+    iterates through loaded video and extracts frames
+    at intervals dictated by save_interal 
+    """
+    frame_count = 0
+    scale = (target_width, target_height)
+    save_interval = int(video_fps / image_per_second)
     while(frame_count < total_video_frames):
         
         valid, image = video.read()
@@ -75,7 +88,7 @@ def image_extraction(video_path = None, image_per_second = 5, target_height = 12
 
         if(valid and frame_count % save_interval == 0):
             image = resize(image, scale)
-            image_name = image_path + "\\" + video_name[0] + "_frame_" + str(frame_count) +r".jpg"
+            image_name = image_path + "\\" + "test" + "_frame_" + str(frame_count) +r".jpg"
             imwrite(image_name, image)
 
         frame_count += 1
