2ff483284d96d350976ceaa52110ac6dff4e0aaf
dermarkr <45435121+dermarkr@users.noreply.github.com>
2020-11-23 13:24:38 -0500

    functionalized image_extraction and added file checks

--- VideoProcessing/imageextraction.py
+++ VideoProcessing/imageextraction.py
@@ -5,66 +5,94 @@ from os import path
 import time
 
 
-# def image_extraction(video_path, image_per_second = 5, target_height = 128, target_width = 256):
 
-video_path = r"C:\Users\Bob\Videos\Project_1.avi" #####Video Source
+# requires that video be AVI format
+def image_extraction(video_path = None, image_per_second = 5, target_height = 128, target_width = 256):
 
-video = VideoCapture(video_path)
+    if video_path == None:
+        print("No video file provided")
+        return False
 
-temp, video_name = os.path.split(video_path)
+    if not path.exists(video_path):
+        print("Invalid Path")
+        return False
 
-video_name = video_name.split(".")
+    if not path.isfile(video_path):
+        print("Input must be file")
+        return False
 
-print(video_name[0])
+    _, video_name = os.path.split(video_path)
 
-total_video_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
-frame_height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
-frame_width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
-video_fps = video.get(cv2.CAP_PROP_FPS)
-print(total_video_frames)
-print(frame_height)
-print(frame_width)
-print(video_fps)
-video_length = total_video_frames / video_fps
-print(video_length)
+    video_name = video_name.split(".")
 
-current_path = os.getcwd()
+    if(video_name[1] != "avi" or video_name[1] != "mkv"):
+        print("Invalid File Format. Inputs most be AVI")
+        return False
 
-if not path.isdir(current_path + r"\images"):
-    os.mkdir(current_path + r"\images")
+    video = VideoCapture(video_path)
 
-image_path = current_path + r"\images"
+    if not video.isOpened():
+        print("Unable to open file. Please ensure input video is either .AVI or .MKV")
+        return False
 
-frame_count = 0
+    total_video_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+    frame_height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    frame_width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+    video_fps = video.get(cv2.CAP_PROP_FPS)
+    print(total_video_frames)
+    print(frame_height)
+    print(frame_width)
+    print(video_fps)
+    video_length = total_video_frames / video_fps
+    print(video_length)
 
-image_per_second = 5 ######Number of frames extracted per second
-target_height = 128
-target_width = 256
+    current_path = os.getcwd()
 
-scale = (target_width, target_height)
+    if not path.isdir(current_path + r"\images"):
+        os.mkdir(current_path + r"\images")
 
-save_interval = video_fps / image_per_second
+    image_path = current_path + r"\images"
 
-begin = time.time()
+    frame_count = 0
 
-while(frame_count < total_video_frames):
-    
-    valid, image = video.read()
+    image_per_second = 5 ######Number of frames extracted per second
+    target_height = 128
+    target_width = 256
 
-    if not valid:
-        print("END")
-        break
+    scale = (target_width, target_height)
 
-    if(valid and frame_count % save_interval == 0):
-        image = resize(image, scale)
-        image_name = image_path + "\\" + video_name[0] + "_frame_" + str(frame_count) +r".jpg"
-        imwrite(image_name, image)
+    save_interval = video_fps / image_per_second
 
-    frame_count += 1
+    begin = time.time()
 
-end = time.time()
+    while(frame_count < total_video_frames):
+        
+        valid, image = video.read()
 
-total_time = end - begin
+        if not valid:
+            print("END")
+            break
 
-print(total_time)
+        if(valid and frame_count % save_interval == 0):
+            image = resize(image, scale)
+            image_name = image_path + "\\" + video_name[0] + "_frame_" + str(frame_count) +r".jpg"
+            imwrite(image_name, image)
 
+        frame_count += 1
+
+    end = time.time()
+
+    total_time = end - begin
+
+    video.release()
+
+    print(total_time)
+
+
+def main():
+    video_path = r"C:\Users\Bob\Videos\Introduction to ML - 1 - What is a ML task.mp4"
+
+    image_extraction(video_path)
+
+if __name__ == '__main__':
+    main()
