c24dcf18865c6009e6cabad584cacdb8e13badda
Steven Xia <stevenxia990430@gmail.com>
2021-03-06 14:58:18 -0500

    Update car_detection.py

--- CarDetectionModel/car_detection.py
+++ CarDetectionModel/car_detection.py
@@ -275,6 +275,88 @@ def draw_bounding_boxes_on_image(image):
     bound_boxes = find_bounding_boxes(mask.copy(), only_bounding_boxes=True)
     return display_masked_image(image.copy(), bound_boxes.copy(), display=False)
 
+def draw_bounding_boxes_on_image_2(t_image, ct, frame):
+	
+   	img_out, contours = draw_bounding_boxes_on_image(t_image, model)
+	for cnt in contours:
+		# draw a bounding box surrounding the object so we can
+		# visualize it
+		(startX, startY, endX, endY) = cv2.boundingRect(cnt)
+		# if cv2.contourArea(cnt) > 0.6 * endX * endY and
+		if cv2.contourArea(cnt) > 150:
+		# if cv2.contourArea(cnt) > 50:
+			endX = startX + endX
+			endY = startY + endY
+			if (endX >= 0 and endX <= width and endY >=0 and endY <= height and startX >=0 and startX <= width and startY >=0 and startY <= height):
+				rects.append((startX, startY, endX, endY))
+
+				cv2.rectangle(frame, (startX, startY),(endX, endY),
+							  (0, 255, 0), 1)
+
+	objects, object_direction,road_directions = ct.update(rects)
+	for (objectID, centroid) in objects.items():
+		# draw both the ID of the object and the centroid of the
+		# object on the output frame
+		text = "Car {}".format(objectID)
+		cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 255, 0), 1)
+		cv2.circle(frame, (centroid[0], centroid[1]), 1, (0, 255, 0), 1)
+
+		if object_direction[objectID] != 0:
+			cv2.arrowedLine(frame, (centroid[0], centroid[1]), (centroid[0]+object_direction[objectID][0], centroid[1]+object_direction[objectID][1]),
+							(255,0,0), 1, tipLength=0.5)
+			if objectID not in car_past_direction:
+				car_past_direction[objectID] = []
+
+
+
+		# img_out = np.transpose(img_out, (2, 0, 1))
+	# plt.imshow(img_out)
+	# plt.show()
+	frame = cv2.resize(frame, (width, height))
+
+	total_num_of_car = ct.numberOfObjects()
+	totalCarText = "Total # of Cars: "+ str(total_num_of_car)
+	currentCarText = "Current # of Cars: " + str(len(objects))
+	cv2.putText(frame, totalCarText, (0, 255), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1)
+	cv2.putText(frame, currentCarText, (0, 235), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+
+	trafficColor = (0, 255, 0) # green
+
+	if (len(objects) > 10): # Red
+		trafficColor = (0, 0, 255)
+	elif (len(objects) > 7): # Orange
+		trafficColor = (0, 144, 255)
+
+	# Traffic Conditions
+	# cv2.putText(frame, "Traffic Condition", (400, 220), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 1)
+	# cv2.rectangle(frame, (400, 230), (512, 255), trafficColor, -1)
+
+	if (len(road_directions) >0):
+		cv2.putText(frame, "Direction", (400, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 1)
+
+		cv2.arrowedLine(frame, (480, 190),
+						(480+ int(road_directions[0][0][0]*15),190 +  int(road_directions[0][0][1]*15)),
+						(255, 0, 0), 1, tipLength=0.5)
+		print(road_directions[0][1])
+		cv2.putText(frame, str(int(road_directions[0][1])), (495, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 1)
+
+		if (len(road_directions) > 1):
+			cv2.putText(frame, "Direction", (400, 230), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 1)
+
+			cv2.arrowedLine(frame, (480, 220),
+							(480 + int(road_directions[1][0][0] * 15), 220 + int(road_directions[1][0][1] * 15)),
+							(255, 0, 0), 1, tipLength=0.5)
+			cv2.putText(frame, str(road_directions[1][1]), (495, 230), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 1)
+
+		if (len(road_directions) > 2):
+			cv2.putText(frame, "Direction", (400, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 1)
+
+			cv2.arrowedLine(frame, (480, 240),
+							(480 + int(road_directions[2][0][0] * 15), 240 + int(road_directions[2][0][1] * 15)),
+							(255, 0, 0), 1, tipLength=0.5)
+			cv2.putText(frame, str(road_directions[2][1]), (495, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 1)		#cv2.rectangle(frame, (400, 230), (512, 255), trafficColor, -1)
+	return frame
 # # example usage
 # image = Image.open(streets_dataset_filepath + '/images/Aptakisic at Bond IP East-9.jpg')
 # t_image = transform_image_for_model(image)
